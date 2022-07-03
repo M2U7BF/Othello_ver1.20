017e25ddfa8dfda5c51ff324a41664c0203894a6
M2U7BF <yhvh4321@gmail.com>
2022-07-03 18:19:03 +0900

    [作成]computerの配置する位置をより有利にする機能を作成

--- src/player/Computer.java
+++ src/player/Computer.java
@@ -16,28 +16,28 @@ public class Computer extends PlayerBase {
 	}
     //行動
     public void turnAction(Computer computer,Me me,GamingView gamingView) {
-            	int[] decidePosition = decidePosition(computer, me);
-            	if(decidePosition[0] == 8 && decidePosition[1] == 8) {
-            		computer.Pass(computer, me);
-            		gamingView.computerPassesLabel.setText(computer.name + "のパス回数 : " + String.valueOf(computer.getPasses()));
-            	} else if(
+    	int[] decidePosition = decidePosition(computer, me);
+    	if(decidePosition[0] == 8 && decidePosition[1] == 8) {
+    		computer.Pass(computer, me);
+    		gamingView.computerPassesLabel.setText(computer.name + "のパス回数 : " + String.valueOf(computer.getPasses()));
+    	} else if(
 //            			true //テスト用
-            			(boolean) canPlacing(decidePosition, computer, me).get("result")
-            			){
-            		Map<String,Object> values = canPlacing(decidePosition, computer, me);
-                	ArrayList<ArrayList<int[]>> turnOverList = (ArrayList<ArrayList<int[]>>) values.get("turnPosition");
-                	
-                    gamingView.placeAnimation(decidePosition,computer);
-                    gamingView.turnOverAnimation(computer, me, turnOverList, gamingView);
-                    
-                	computer.placing(decidePosition, computer, me,
-            				turnOverList);
-            	} else {
-            		computer.Pass(computer, me);
-            		System.out.println("Cpmputer : エラー : 探索した座標にはルール上、置けません("+String.valueOf(decidePosition[0])+","+String.valueOf(decidePosition[1])+")");
-            		gamingView.computerPassesLabel.setText(computer.name + "のパス回数 : " + String.valueOf(computer.getPasses()));
-            	}
-            }
+    			(boolean) canPlacing(decidePosition, computer, me).get("result")
+    			){
+    		Map<String,Object> values = canPlacing(decidePosition, computer, me);
+        	ArrayList<ArrayList<int[]>> turnOverList = (ArrayList<ArrayList<int[]>>) values.get("turnPosition");
+        	
+            gamingView.placeAnimation(decidePosition,computer);
+            gamingView.turnOverAnimation(computer, me, turnOverList, gamingView);
+            
+        	computer.placing(decidePosition, computer, me,
+    				turnOverList);
+    	} else {
+    		computer.Pass(computer, me);
+    		System.out.println("Cpmputer : エラー : 探索した座標にはルール上、置けません("+String.valueOf(decidePosition[0])+","+String.valueOf(decidePosition[1])+")");
+    		gamingView.computerPassesLabel.setText(computer.name + "のパス回数 : " + String.valueOf(computer.getPasses()));
+    	}
+    }
 
     // 決める
     public int[] decidePosition(PlayerBase me, PlayerBase enemy) {
@@ -69,7 +69,8 @@ public class Computer extends PlayerBase {
         }
 
         if (arrays2.size() > 0) {
-        	decidePosition = randomChoice(arrays2);
+        	decidePosition = ChoicePosition(arrays2);
+//        	decidePosition = randomChoice(arrays2);
         } else if (arrays2.size() == 0) {
             // 置けるマスが無い場合
             decidePosition[0] = 8;
@@ -78,6 +79,70 @@ public class Computer extends PlayerBase {
 
         return decidePosition;
     }
+    
+    public int[] ChoicePosition(ArrayList<ArrayList<int[]>> arrays2) {
+    	boolean doseExit = false;
+    	int[] decidePosition = new int[2];
+    	
+    	Random rnd = new Random();
+    	
+    	ArrayList<int[]> arrays5 = new ArrayList<>();
+    	
+    	// 3重リストのarray2の1つ目を展開
+    	//置ける場所をすべてarraylistにまとめる
+        ArrayList<int[]> arrays4 = new ArrayList<>();
+        for(int i=0; i < arrays2.size(); i++ ) {
+        	for(int j=0; j < arrays2.get(i).size(); j++) {
+        		arrays4.add(arrays2.get(i).get(j));
+        	}
+        }
+        
+        //置ける場所から優先順位をつける
+        
+      //角をとる
+        for(int i=0; i < arrays4.size(); i++) {
+        	int x = arrays4.get(i)[0];
+        	int y = arrays4.get(i)[1];
+        	if((x==0 && y==0)||(x==0 && y==7)||(x==7 && y==0)||(x==7 && y==7)) {
+        		arrays5.add(arrays4.get(i));
+        		doseExit = true;
+        		continue;
+        	}
+        }
+        
+        //最も外側のマスをとる
+        if(!(doseExit)) {
+	        for(int i=0; i < arrays4.size(); i++) {
+	        	int x = arrays4.get(i)[0];
+	        	int y = arrays4.get(i)[1];
+	        	if((x==3 || x==5)&&(y==0 || y==7)||(y==2 && y==4)&&(x==0 || x==7)) {
+	        		arrays5.add(arrays4.get(i));
+	        		doseExit = true;
+	        		continue;
+	        	}
+	        }
+        }
+    	
+        if(!(doseExit)) {
+	        for(int i=0; i < arrays4.size(); i++) {
+	        	int x = arrays4.get(i)[0];
+	        	int y = arrays4.get(i)[1];
+	        	if((x>=2 && x<=5)&&(y>=2 && y<=5)) {
+	        		arrays5.add(arrays4.get(i));
+	        		doseExit = true;
+	        		continue;
+	        	}
+	        }
+        }
+        
+        if(doseExit){
+        	decidePosition = arrays5.get(rnd.nextInt(arrays5.size()));
+        }else{
+        	decidePosition = randomChoice(arrays2);
+        }
+        
+    	return decidePosition;
+	}
     
     public int[] randomChoice(ArrayList<ArrayList<int[]>> arrays2) {
     	int[] decidePosition = new int[2];
