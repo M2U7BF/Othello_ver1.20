6716e2f28b5aa445453a166492bf058b157c2704
M2U7BF <yhvh4321@gmail.com>
2023-12-02 13:34:55 +0900

    refactor: ファイルパスを改善

--- src/judge/Judge.java
+++
@@ -1,7 +0,0 @@
-package judge;
-
-public class Judge {
-	public int Scoring() {
-		return 0;
-	}
-}
--- src/player/Me.java
+++
@@ -1,8 +0,0 @@
-package player;
-
-public class Me extends PlayerBase {
-    public Me() {
-    	this.name = "自分";
-    	this.id = 0;
-    }
-}
